937579b42dc14e1f68f7e521d037209dae5717a2
mo <1418968663@qq.com>
2017-09-30 00:44:11 +0800

    基础配置

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,18 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+    hello world
+    hello world
+    hello world
   </div>
 </template>
 
-<script>
-export default {
-  name: 'app'
-}
+<script type="text/ecmascript-6">
+
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped long="stylus" rel="stylesheet/stylus">
+  @import '~common/stylus/variable'
+
+  /* #app
+    color $color-theme */
 </style>
