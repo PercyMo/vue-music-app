642988fcc22fe4cdd0ca5d728527eaeecd0d9082
mo <1418968663@qq.com>
2017-10-11 23:02:52 +0800

    导航路由配置

--- src/components/m-header/m-header.vue
+++ src/components/m-header/m-header.vue
@@ -15,5 +15,22 @@
     @import "~common/stylus/variable"
     @import "~common/stylus/mixin"
     .m-header
+        height 44px
         position relative
+        color $color-theme
+        text-align center
+        font-size 0
+    .m-header-icon
+        margin 6px 9px 0 0
+        width 30px
+        height 32px
+        display inline-block
+        vertical-align top
+        bg-image('logo')
+        background-size 30px 32px
+    .m-header-title
+        display inline-block
+        font-size $font-size-large
+        line-height 44px
+        vertical-align top
 </style>
