24db0ab8fd62a39f0abca2a3350f088eb9e25b00
mo <1418968663@qq.com>
2017-11-07 23:20:52 +0800

    1

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -31,11 +31,15 @@
     @import "~common/stylus/variable"
     
     .singer-detail
-        width 100%
         position fixed
         top 0
         left 0
+        right 0
         bottom 0
         background $color-background
         z-index 100
+        &.slide-enter-active, &.slide-leave-active
+            transition all .3s
+        &.slide-enter, &.slide-leave-to
+            transform translate3d(100%, 0, 0)
 </style>
