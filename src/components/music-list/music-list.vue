2b3895bf4a88763edf66a94d6fde9dcb47c2baa8
maobangxin <1418968663@qq.com>
2017-11-09 17:58:19 +0800

    歌手详情数据

---
+++ src/components/music-list/music-list.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="music-list">
+        撒发发 
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+    export default {
+        props: {
+            songs: {
+                type: Array,
+                default: []
+            }
+        },
+        data() {
+            return {
+            }
+        },
+        created() {
+        },
+        methods: {
+            
+        },
+        components: {
+        }
+    }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+    @import "~common/stylus/variable"
+    
+    .music-list
+        position fixed
+        top 0
+        left 0
+        right 0
+        bottom 0
+        background $color-background
+        z-index 100
+</style>
--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,18 +1,20 @@
 <template>
     <transition name="slide">
-        <div class="singer-detail">
-            歌手详情页面
-        </div>
+        <music-list :songs="songs"></music-list>
     </transition>
 </template>
 
 <script type="text/ecmascript-6">
+    import MusicList from 'components/music-list/music-list'
+    import {getSingerDetail} from 'api/singer'
+    import {ERR_OK} from 'api/config'
+    import {createSong} from 'common/js/song'
     import {mapGetters} from 'vuex'
 
     export default {
         data() {
             return {
-                mao: {}
+                songs: []
             }
         },
         computed: {
@@ -21,29 +23,40 @@
             ])
         },
         created() {
-            console.log(this.singer)
+            this._getDetail()
         },
         methods: {
-            
+            _getDetail() {
+                if (!this.singer.id) {
+                    this.$router.push('/singer')
+                    return
+                }
+                getSingerDetail(this.singer.id).then((res) => {
+                    if (res.code === ERR_OK) {
+                        this.songs = this._normalizeSongs(res.data.list)
+                    }
+                })
+            },
+            _normalizeSongs(list) {
+                let ret = []
+                list.forEach((item) => {
+                    let {musicData} = item
+                    if (musicData.songid && musicData.albummid) {
+                        ret.push(createSong(musicData))
+                    }
+                })
+                return ret
+            }
         },
         components: {
+            MusicList
         }
     }
 </script>
 
-<style lang="stylus" rel="stylesheet/stylus">
-    @import "~common/stylus/variable"
-    
-    .singer-detail
-        position fixed
-        top 0
-        left 0
-        right 0
-        bottom 0
-        background $color-background
-        z-index 100
-        &.slide-enter-active, &.slide-leave-active
-            transition all .3s
-        &.slide-enter, &.slide-leave-to
-            transform translate3d(100%, 0, 0)
+<style scoped lang="stylus" rel="stylesheet/stylus">
+    &.slide-enter-active, &.slide-leave-active
+        transition all .3s
+    &.slide-enter, &.slide-leave-to
+        transform translate3d(100%, 0, 0)
 </style>
