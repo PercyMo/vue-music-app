ca58e29935647d3f8eaf9670df6b859efaf6431b
maobangxin <1418968663@qq.com>
2017-11-10 19:19:03 +0800

    icon图标

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -1,10 +1,9 @@
 <template>
     <div class="music-list">
         <div class="back" @click="back">
-            <i class="icon-playlist"></i>
-            <!-- <img width="100%" :src="bgImage"> -->
+            <i class="icon-back"></i>
         </div>
-        <h1 class="title">{{title}}</h1>
+        <h1 class="title" v-html="title"></h1>
     </div>
 </template>
 
@@ -42,6 +41,7 @@
 
 <style lang="stylus" rel="stylesheet/stylus">
     @import "~common/stylus/variable"
+    @import "~common/stylus/mixin"
     
     .music-list
         position fixed
@@ -50,9 +50,32 @@
         right 0
         bottom 0
         background $color-background
+        background #666
         z-index 100
-        i
-            margin 10px
-            display block
-            font-size 24px
+        .back
+            position absolute
+            top 0
+            left 6px
+            z-index 50
+            i
+                padding 0 10px
+                height 50px
+                display block
+                color $color-text-w
+                font-size $font-size-large-x
+                line-height 50px
+        .title
+            padding 0 60px
+            height 50px
+            position absolute
+            top 0
+            left 0
+            width 100%
+            no-wrap()
+            color $color-text-w
+            font-size $font-size-large
+            text-align left
+            line-height 50px
+            z-index 40
+            box-sizing border-box
 </style>
