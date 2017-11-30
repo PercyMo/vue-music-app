cc873e1aa96a76c5269c72cf38ae8bc9469c9071
maobangxin <1418968663@qq.com>
2017-11-30 19:45:27 +0800

    数组随机置乱

---
+++ src/common/js/util.js
@@ -0,0 +1,12 @@
+export function shuffle(arr) {
+    //slice()方法不传参数，将返回数组的一个浅拷贝
+    //同样实现的，还有concat()方法
+    let _arr = arr.slice()
+    for (let i = 0; i < _arr.length; i++) {
+        let j = Math.floor(Math.random() * (i + 1))
+        let t = _arr[i]
+        _arr[i] = _arr[j]
+        _arr[j] = t
+    }
+    return _arr
+}
