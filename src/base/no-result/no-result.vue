ac7a8aa89577e5def821ccabb5aae076a1c0da5e
maobangxin <1418968663@qq.com>
2017-12-31 17:42:57 +0800

    1

---
+++ src/base/no-result/no-result.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="no-result">
+        <div class="no-result-icon"></div>
+        <p class="no-result-text">{{title}}</p>
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+    export default {
+        props: {
+            title: {
+                type: String,
+                default: ''
+            }
+        }
+    }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+</style>
