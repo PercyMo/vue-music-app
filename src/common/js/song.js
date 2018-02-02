0f4a020b7a202aefb60cfd151caf64a55b04cb94
maobangxin <1418968663@qq.com>
2018-02-02 18:25:15 +0800

    临时路径

--- src/common/js/song.js
+++ src/common/js/song.js
@@ -42,7 +42,8 @@ export function createSong(musicData) {
         album: musicData.albumname,
         duration: musicData.interval,
         image: `https://y.gtimg.cn/music/photo_new/T002R300x300M000${musicData.albummid}.jpg?max_age=2592000`,
-        url: `http://ws.stream.qqmusic.qq.com/${musicData.songid}.m4a?fromtag=46`
+        // url: `http://ws.stream.qqmusic.qq.com/${musicData.songid}.m4a?fromtag=46`
+        url: 'http://dl.stream.qqmusic.qq.com/C400000ryYx71hFmdF.m4a?vkey=52A2F0D14EE1C0EFBC8620A272790F2E0DED3D022E39F77D06719AD7FE5567CDBACBA2F4550F14B6351B6C385EB93ADE943A7AB90AB36554&guid=5603704467&uin=1418968663&fromtag=66'
     })
 }
 
